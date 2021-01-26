f5df135972f82ea6303e54667329a119e7ba6d5f
Fernando H Correa <fernandohcorrea@gmail.com>
2021-01-26 10:26:19 -0300

    20210126 - Pequenas correções

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,6 @@ import QuizBackground from '../components/QuizBackGround'
 import Footer from '../components/Footer';
 import GitHubCorner from '../components/GitHubCorner';
 
-// const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `;
-
 export const QuizContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -37,13 +30,13 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
-        <title>AluraQuiz - Modelo Base</title>
+        <title>{db.title}</title>
       </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>The legend of PHP</h1>
+            <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
             <form onSubmit={function (infosDoEvento) {
@@ -78,7 +71,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl="https://github.com/fernandohcorrea/alura-quizreact" />
     </QuizBackground>
   );
 }
